c89db2f80ff65664a7455bede03159fef92e5058
munaray <munahdesigns@gmail.com>
2024-09-28 15:48:08 +0100

    Implementig Swagger Documentation Configuration for both typescript and javascript

--- src/index.js
+++ src/index.js
@@ -12,22 +12,22 @@ const program = new Command();
 
 program
   .name("create-node-app")
-  .description("CLI to scaffold a Node.js project with database and ORM setup")
+  .description(
+    "CLI to scaffold a Node.js project with database, ORM and swagger ui documentation setup"
+  )
   .version("1.0.0");
 
 program
   .command("new <project-name>")
   .description("create a new Node.js project")
-  .action(async (projectName: string) => {
-    const spinner = ora("Initializing project...").start();
+  .action(async (projectName) => {
     const projectPath = path.join(process.cwd(), projectName);
+    let spinner;
 
     try {
       // Check if project folder already exists
       if (fs.existsSync(projectPath)) {
-        spinner.fail(
-          chalk.red(`Project folder ${projectName} already exists.`)
-        );
+        console.log(chalk.red(`Project folder ${projectName} already exists.`));
         return;
       }
 
@@ -64,12 +64,37 @@ program
         ]
       );
 
-      spinner.text = `Creating project folder ${projectName}`;
+      // Start spinner only after prompt
+      spinner = ora("Initializing project...").start();
+
+      // Create project folder and initialize npm project
       fs.mkdirSync(projectPath);
       process.chdir(projectPath);
-      execSync("npm init -y", { stdio: "inherit" });
 
-      // Create basic project structure in src folder
+      spinner.text = "Running npm init...";
+      execSync("npm init -y", { stdio: "inherit" });
+      fs.writeFileSync(
+        "package.json",
+        JSON.stringify({
+          name: `${projectName}`,
+          version: "1.0.0",
+          main: "server.js",
+          module: "NodeNext",
+          scripts: {
+            test: 'echo "Error: no test specified" && exit 1',
+            build: "tsc --build",
+            start: "node ./build/server.js",
+            dev: "nodemon ./src/server.ts",
+            lint: "eslint",
+          },
+          keywords: [],
+          author: "",
+          license: "ISC",
+          description: "",
+        })
+      );
+
+      // Create basic project structure
       const srcDirs = [
         "controllers",
         "routes",
@@ -84,7 +109,7 @@ program
       fs.mkdirSync("src");
       srcDirs.forEach((dir) => fs.mkdirSync(`src/${dir}`));
 
-      // Add basic files
+      // Create basic files
       const createBasicFiles = () => {
         fs.writeFileSync(
           ".env.sample",
@@ -117,12 +142,11 @@ program
         SMTP_SERVICES =
         SMTP_MAIL =
         SMTP_PASSWORD =
-
         `
         );
         fs.writeFileSync(".gitignore", "node_modules/\n.env\nbuild/\ndist/\n");
         fs.writeFileSync(
-          ".prettierrc",
+          ".prettierrc.json",
           JSON.stringify({
             semi: true,
             singleQuote: false,
@@ -171,6 +195,7 @@ export default [
 
       if (useTypescript) {
         spinner.text = "Setting up TypeScript...";
+        console.log("Setting up TypeScript...");
         execSync(
           "npm install typescript @types/node ts-node nodemon --save-dev",
           { stdio: "inherit" }
@@ -199,15 +224,18 @@ export default [
             2
           )
         );
+        console.log("TypeScript setup completed!");
       } else {
         fs.writeFileSync(
           "src/index.js",
           "// Entry point\nconsole.log('Hello JavaScript!');"
         );
         execSync("npm install nodemon --save-dev", { stdio: "inherit" });
+        console.log("JavaScript setup completed!");
       }
 
       spinner.text = `Installing ${framework} framework...`;
+
       if (framework === "Nest") {
         execSync("npm install @nestjs/core @nestjs/common rxjs dotenv", {
           stdio: "inherit",
@@ -226,33 +254,32 @@ import { Module } from '@nestjs/common';
 export class AppModule {}
 `
         );
-      } else {
+      } else if (framework === "Express") {
+        spinner.text = "Setting up your express app...";
         execSync(
-          `npm install ${framework.toLowerCase()} dotenv cors cookie-parser swagger-ui-express `,
+          `npm install express dotenv cors cookie-parser swagger-ui-express yamljs`,
           {
             stdio: "inherit",
           }
         );
-        if (framework === "Express") {
-          fs.writeFileSync(
-            useTypescript ? "src/app.ts" : "src/app.js",
-            `
-import ${framework.toLowerCase()}, {Request, Response} from '${framework.toLowerCase()}';
+        console.log("Adding app.ts or app.js config file");
+        fs.writeFileSync(
+          useTypescript ? "src/app.ts" : "src/app.js",
+          `
+import express, { Request, Response } from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import "dotenv/config";
-import {middlewareErrorHandler} from "./middleware/error"
-
-// Pls make sure to add all your route end point into index.ts inside route folder
+import { middlewareErrorHandler } from "./middleware/error";
 import routes from "./routes/index";
 
 import swaggerUi from "swagger-ui-express";
-import OPENAPI_DOCS from "./swagger-docs/swagger";
+import OPENAPI_DOCS_SPEC from "./swagger-docs/swagger";
 
-export const app = ${framework.toLowerCase()}();
+export const app = express();
 
 // body parser
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
 
 //cookie parser
 app.use(cookieParser());
@@ -280,18 +307,17 @@ app.get("/api/test", (request: Request, response: Response) => {
 app.all("*", (request: Request, response: Response) => {
   response.status(404).send({
     success: false,
-    // Pls remove the quote from ${"request.originalUrl"} and delete this comment
-    message: ${"request.originalUrl"} route you are trying to reach does not exist,
+    message: \${request.originalUrl} route you are trying to reach does not exist,
   });
 });
 
 app.use(middlewareErrorHandler);
-
 `
-          );
-          fs.writeFileSync(
-            "src/server.ts",
-            `
+        );
+        console.log("Adding server.ts or server.js config file");
+        fs.writeFileSync(
+          useTypescript ? "src/server.ts" : "src/server.js",
+          `
 import { app } from "./app";
 import "dotenv/config";
 import connectDB from "./utils/db";
@@ -299,15 +325,131 @@ import connectDB from "./utils/db";
 const PORT = process.env.PORT || 3001;
 // create server
 app.listen(PORT, () => {
-  console.log(Server is connected at port ${"PORT"});
+  console.log(Server is connected at port \${PORT});
   connectDB();
 });
 `
-          );
-        }
+        );
+
+        spinner.text = "Configuring Swagger UI Documentation...";
+        fs.mkdirSync("src/swagger-docs/swagger-api-docs");
+        fs.mkdirSync("src/swagger-docs/swagger-schema-docs");
+
+        fs.writeFileSync(
+          useTypescript
+            ? "src/swagger-docs/swagger.ts"
+            : "src/swagger-docs/swagger.js",
+          `
+import YAML from "yamljs";
+import path from "path";
+import fs from "fs";
+
+const yamlAPIDirectory = path.join(__dirname, "swagger-api-docs");
+const yamlSchemaDirectory = path.join(__dirname, "swagger-schema-docs");
+
+const yamlAPIDocuments: Record<string, any> = {};
+const yamlSchemaDocuments: Record<string, any> = {};
+
+// Read all files from the swagger-api-docs directory
+fs.readdirSync(yamlAPIDirectory).forEach((file) => {
+  // Filter only .yaml or .yml files
+  if (file.endsWith(".yaml") || file.endsWith(".yml")) {
+    const filePath = path.join(yamlAPIDirectory, file);
+
+    // Using the file as a key without the extension
+    const fileNameWithoutExtension = path.basename(file, path.extname(file));
+    try {
+      yamlAPIDocuments[fileNameWithoutExtension] = YAML.load(filePath);
+    } catch (error: any) {
+      console.error(Error loading \${file}, error);
+    }
+  }
+});
+
+// Read all files from the swagger-schema-docs directory
+fs.readdirSync(yamlSchemaDirectory).forEach((file) => {
+  // Filter only .yaml or .yml files
+  if (file.endsWith(".yaml") || file.endsWith(".yml")) {
+    const filePath = path.join(yamlSchemaDirectory, file);
+
+    // Using the file as a key without the extension
+    const fileNameWithoutExtension = path.basename(file, path.extname(file));
+    try {
+      yamlSchemaDocuments[fileNameWithoutExtension] = YAML.load(filePath);
+    } catch (error: any) {
+      console.error(Error loading \${file}, error);
+    }
+  }
+});
+
+const sampleAPI = yamlAPIDocuments["sample-api"];
+const sampleSchema = yamlSchemaDocuments["sample-schema"];
+
+const OPENAPI_DOCS_SPEC = {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Create-Node-App CLI tools",
+    description: CLI to scaffold a Node.js project with database, ORM and swagger ui documentation setup
+                ,
+  },
+  paths: {
+    ...sampleAPI.paths,
+  },
+  components: {
+    schemas: {
+      ...sampleSchema.components.schemas,
+    },
+  },
+};
+
+export default OPENAPI_DOCS_SPEC;
+
+          `
+        );
+        fs.writeFileSync(
+          "src/swagger-docs/swagger-api-docs/sample-api.yaml",
+          `# Sample API YAML File
+
+# This is a sample api YAML file (sample-api.yaml) to demonstrate the structure for documenting your API.
+
+# To organize your api effectively, create separate YAML files for each API Controller (e.g., user-api.yaml, course-api.yaml), handling specific responsibilities for users and courses endpoints respectively.
+
+# Integrate these YAML files into your swagger.ts or swagger.js file to consolidate your API documentation.
+
+# Example:
+
+# // swagger.ts or swagger.js
+# const userAPI = yamlAPIDocuments["user-api"];
+
+# follow this method to integrate all your yaml files`
+        );
+        fs.writeFileSync(
+          "src/swagger-docs/swagger-schema-docs/sample-schema.yaml",
+          `# Sample Schema YAML File
+
+# This is a sample schema YAML file (sample-schema.yaml) to demonstrate the structure for documenting your API.
+
+# To organize your schemas effectively, create separate YAML files for each schema (e.g., user-schema.yaml, course-schema.yaml), each handling specific responsibilities.
+
+# Import these YAML files into your swagger.ts or swagger.js file to consolidate your API documentation.
+
+# Example:
+
+# // swagger.ts or swagger.js
+
+const userSchema = yamlSchemaDocuments["sample-schema"];
+follow this method to integrate all your yaml files
+`
+        );
+      } else {
+        execSync(`npm install ${framework.toLowerCase()}`, {
+          stdio: "inherit",
+        });
       }
 
       spinner.text = `Installing ${orm} and setting up ${database}...`;
+      console.log(`Installing ${orm} and setting up ${database}...`);
       if (orm === "Mongoose") {
         execSync("npm install mongoose", { stdio: "inherit" });
         fs.writeFileSync(
@@ -321,9 +463,8 @@ const dbUrl: string = process.env.MONGODB_URL || "";
 const connectDB = async () => {
   try {
     await mongoose.connect(dbUrl).then((data) => {
-    // Pls remove the quote around data.connection.host
-      console.log(Database connected to ${"data.connection.host"});
-    });
+      console.log(Database connected to \${data.connection.host});
+      });
   } catch (error: any) {
     console.log(error.message);
     setTimeout(connectDB, 5000);
@@ -331,7 +472,6 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
 `
         );
       } else if (orm === "Prisma") {
@@ -339,12 +479,19 @@ export default connectDB;
         execSync("npx prisma init", { stdio: "inherit" });
       }
 
+      // Initialize git and make the first commit
+      spinner.text = "Initializing git repository...";
+      execSync("git init", { stdio: "inherit" });
+      execSync("git add .", { stdio: "inherit" });
+      execSync('git commit -m "Initial commit from Node app"', {
+        stdio: "inherit",
+      });
+
       spinner.succeed(
         chalk.green(`Project ${projectName} created successfully!`)
       );
       console.log(chalk.blue("Next steps:"));
       console.log(`1. ${chalk.green(`cd ${projectName}`)}`);
-      console.log(`2. ${chalk.green("npm install")}`);
       console.log(
         `3. ${chalk.green("npm run dev")} to start development server`
       );
